47fb5141fe07f4f27e2700199824da09484332d9
dherv <damien.herv@gmail.com>
2021-05-16 08:45:22 +0900

    chore(storybook): remove theme add on
    add on blocks chromatic

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,6 @@
-import { addDecorator } from "@storybook/react";
-import { withThemesProvider } from "storybook-addon-styled-component-theme";
-import { ThemeProvider } from "styled-components";
-import {theme} from "../src/styled/lib/styled.themes"
 import {GlobalStyle} from "../src/styled/lib/styled.globals"
 import "destyle.css"
 
-const themes = [theme];
-addDecorator(withThemesProvider(themes), ThemeProvider);
 
 export const decorators = [
   Story => (
